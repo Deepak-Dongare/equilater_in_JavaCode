5e8ecea277655910fdda60f404b3b66779f55d91
Deepak Dongare <52232024+Deepak-Dongare@users.noreply.github.com>
2019-08-08 12:49:43 -0700

    equilater_in_

---
+++ equilater.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class equilater {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the lenght of any side");
+		a =sc.nextInt();
+		double Area=0.433*Math.pow(a,2);
+		System.out.println(Area);
+		
+
+	}
+
+}
